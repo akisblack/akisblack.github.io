9d64a6ee29387ea76688eece332d45ae3d4f9af1
Oxygemo <alej0hio2007@gmail.com>
2021-07-06 21:18:18 +0200

    Remove webring until Chelonian renews url
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -16,9 +16,6 @@
 	let github = "img/git.svg";
 	export let green;
 	export let white;
-    let webringPrev = "https://stephenvk.xyz/";
-    let webringAll = "https://stephenvk.xyz/ring";
-    let webringNext = "https://devnol.github.io/";
 </script>
 
 <main>
@@ -45,10 +42,4 @@
             </ul>
         </div>
     </div>
-
-<div id="webring">
-    <a style="color:{white};" href={webringPrev}>&lt;--</a>
-    <a style="color:{green};" href={webringAll}>Zoomer Hacking</a>
-    <a style="color:{white};" href={webringNext}>--&gt;</a>
-</div>
 </main>
