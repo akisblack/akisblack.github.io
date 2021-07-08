7d241f2c982203399435d2792eef7a3edfe9c82a
akisblack <mavrommatisakis@gmail.com>
2021-07-08 17:34:00 +0300

    make webring a component

--- src/App.svelte
+++ src/App.svelte
@@ -16,9 +16,7 @@
 	let github = "img/git.svg";
 	export let green;
 	export let white;
-    let webringPrev = "https://stephenvk.xyz/";
-    let webringAll = "https://stephenvk.xyz/ring";
-    let webringNext = "https://devnol.github.io/";
+	import Webring from './components/Webring.svelte';
 </script>
 
 <main>
@@ -45,10 +43,5 @@
             </ul>
         </div>
     </div>
-
-<div id="webring">
-    <a style="color:{white};" href={webringPrev}>&lt;--</a>
-    <a style="color:{green};" href={webringAll}>Zoomer Hacking</a>
-    <a style="color:{white};" href={webringNext}>--&gt;</a>
-</div>
+<Webring />
 </main>
