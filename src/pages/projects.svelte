249ac56360a30e4030e273e24f8709ba6d2e1de9
akisblack <mavrommatisakis@gmail.com>
2021-07-09 21:46:55 +0300

    maybe fix the weird bug

--- src/pages/projects.svelte
+++ src/pages/projects.svelte
@@ -1,4 +1,5 @@
 <svelte:head>
+    <link rel="stylesheet" href="../css/Projects.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<title>Projects | akisblack</title>
@@ -11,7 +12,6 @@
 	<meta name="description" content="akisblack's site">
 	<meta name="author" content="akisblack">
 	<link rel="icon" type="image/png" href="./img/favicon.png">
-    <link rel="stylesheet" href="../css/Projects.css">
 </svelte:head>
 
 <script>
