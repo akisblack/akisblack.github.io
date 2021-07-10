c733e088298391cb83f7d94eaddd4aa9415c074e
akisblack <mavrommatisakis@gmail.com>
2021-07-10 18:03:38 +0300

    aaaaaaaaaaaaaaaaa

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<link rel="stylesheet" href="../css/Index.css">
+	<link rel="stylesheet" href="../css/index.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<title>Home | akisblack</title>
