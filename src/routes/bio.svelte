4081797f73ee1d5f63aa59301f25f9c635eba26b
akisblack <mavrommatisakis@gmail.com>
2021-07-16 22:19:59 +0300

    hmmm

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<link rel="stylesheet" href="./css/pages.css">
+	<link rel="stylesheet" href="/css/pages.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<title>Bio | akisblack</title>
