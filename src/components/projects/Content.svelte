7cf681b8486bbccf2e0122c9012ed8475898899e
akisblack <mavrommatisakis@gmail.com>
2021-07-16 19:10:47 +0300

    add fair license to projects

--- src/components/projects/Content.svelte
+++ src/components/projects/Content.svelte
@@ -1,6 +1,9 @@
 <script>
     let websiteText = "I am using this website as a chance to make my Svelte skills better.";
+    let fairLicenseText = "The Fair License is an OSS license made to be simple and have a good amount of restrictions as well."
 </script>
 
 <h2 class="project-name">This Website</h2>
 <h2 class="project-text">{websiteText}</h2>
+<h2 class="project-name">Fair License</h2>
+<h2 class="project-text">{fairLicenseText}</h2>
