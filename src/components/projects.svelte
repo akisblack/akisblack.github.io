9c10b4dd4622eed7fcce67c8c3ec19a6008e0241
akisblack <mavrommatisakis@gmail.com>
2021-08-25 23:19:15 +0300

    use props for projects (thanks to devnol)

---
+++ src/components/projects.svelte
@@ -0,0 +1,8 @@
+<div class="container">
+	<a href={project.url} class="project-name"><i class="{project.icon}"></i> {project.name}</a>
+	<h2 class="project-text">{project.description}</h2>
+</div>
+
+<script>
+	export let project;
+</script>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -11,8 +11,23 @@ of the akisblack Software License, which can be found here: http://github.com/ak
 </svelte:head>
 
 <script>
-    import Content from "$components/projectsContent.svelte";
     import Header from "$components/subpageHeader.svelte";
+    import Project from '$components/projects.svelte';
+
+    const projects =  {
+        "Website": {
+            "name": "This Website",
+            "icon": "mdi mdi-web",
+            "description": "I am using this website as a chance to make my Svelte skills better.",
+            "url": "https://github.com/akisblack/akisblack.github.io",
+        },
+        "License": {
+            "name": "akisblack Software License (ASL)",
+            "icon": "mdi mdi-bank-outline",
+            "description": "A license that I use for my current projects.",
+            "url": "https://github.com/akisblack/akisblack-software-license",
+        }
+    }
 </script>
 
 <main>
@@ -20,7 +35,8 @@ of the akisblack Software License, which can be found here: http://github.com/ak
         <Header />
         <div id="stuff">
 			<h1 class="title">Projects</h1>
-            <Content />
+                <Project project={projects.Website}/>
+                <Project project={projects.License}/>
         </div>
     </div>
 </main>
