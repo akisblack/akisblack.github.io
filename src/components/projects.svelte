3d0cd9935ef7e9bd30be209034e2d5df598b037d
akisblack <mavrommatisakis@gmail.com>
2021-08-26 12:01:03 +0300

    h

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,3 +1,9 @@
+<!--
+Copyright (C) 2021 akisblack
+This work is free. You can redistribute it and/or modify it under the terms
+of the akisblack Software License, which can be found here: http://github.com/akisblack/akisblack-software-license.
+-->
+
 <div class="container">
 	<a href={project.url} class="project-name"><i class="{project.icon}"></i> {project.name}</a>
 	<h2 class="project-text">{project.description}</h2>
