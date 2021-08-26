9872e08e1e9328e6392062275ce778edcf9c8014
akisblack <mavrommatisakis@gmail.com>
2021-08-26 12:43:45 +0300

    make the config smaller

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
 import path from "path"
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
-	// options passed to svelte.compile (https://svelte.dev/docs#svelte_compile)
-	compilerOptions: null,
-
-	// an array of file extensions that should be treated as Svelte components
-	extensions: [".svelte"],
-
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: "./build",
-			assets: "./build",
-			fallback: null
-		}),
-		amp: false,
-		appDir: "./_app",
+		adapter: adapter(),
 		files: {
-			hooks: "./src/hooks",
 			assets: "./src/assets",
-			routes: "./src/routes",
-			template: "./src/app.html"
 		},
-		floc: false,
 		vite: {
 			resolve: {
 			  alias: {
@@ -35,8 +19,6 @@ const config = {
 			}
 		  },
 	},
-	// options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
-	preprocess: null
 };
 
 export default config;
