4a47cd4b109cdf2efa261c467bfd11ead03d44b1
akisblack <mavrommatisakis@gmail.com>
2021-08-27 19:54:05 +0300

    shit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/css/global.css">
 	<link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css" rel="stylesheet">
 	<link rel="icon" href="/img/favicon.png">
 
