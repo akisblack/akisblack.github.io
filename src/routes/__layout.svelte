f632a81fc402c09c42cc142785142486d44c1d58
akisblack <mavrommatisakis@gmail.com>
2021-08-27 19:52:25 +0300

    move head stuff to layout file

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,20 @@
+<svelte:head>
+	<!--Imports and viewport stuff-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css" rel="stylesheet">
+	<link rel="icon" href="/img/favicon.png">
+
+	<!--Embed metas-->
+	<meta property="og:url" content="https://akisblack.github.io">
+	<meta property="og:type" content="article">
+	<meta property="og:title" content="akisblack's site">
+	<meta name="theme-color" content="#ffffff" />
+	<meta name="description" content="akisblack's site">
+	<meta name="author" content="akisblack">
+</svelte:head>
+
 <script>
 	import Footer from "$components/Footer.svelte"
 </script>
