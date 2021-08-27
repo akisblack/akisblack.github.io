81776ea6158c5161c0f11c7241890adee91b1a53
akisblack <mavrommatisakis@gmail.com>
2021-08-27 21:10:14 +0300

    make projects component name capitalised

---
+++ src/components/Projects.svelte
@@ -0,0 +1,14 @@
+<!--
+Copyright (C) 2021 akisblack
+This work is free. You can redistribute it and/or modify it under the terms
+of the akisblack Software License, which can be found here: http://github.com/akisblack/akisblack-software-license.
+-->
+
+<div class="container">
+	<a href={project.url} class="project-name"><i class="{project.icon}"></i> {project.name}</a>
+	<h2 class="desc">{project.description}</h2>
+</div>
+
+<script>
+	export let project;
+</script>
