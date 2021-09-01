a784bbca31907adc5ade3c59ddae3d70a9bd4484
akisblack <mavrommatisakis@gmail.com>
2021-09-01 20:08:06 +0300

    i hate footers

---
+++ src/lib/Header.svelte
@@ -0,0 +1,34 @@
+<footer>
+	<a href="/"><i class="mdi mdi-home-outline"></i> Home</a>
+	<a href="../bio"><i class="mdi mdi-help-circle-outline"></i> Bio</a>
+	<a href="../projects"><i class="mdi mdi-wrench-outline"></i> Projects</a>
+	<a href="../blog"><i class="mdi mdi-book-outline"></i> Blog</a>
+	<hr>
+	<p>Made with <a href="https://kit.svelte.dev">SvelteKit</a>and <span>&lt;3</span> by Akis.</p>
+</footer>
+
+<style lang="postcss">
+	footer {
+		width: 99%;
+		background-color: var(--pfp-color);
+		padding: 1em 0 1em 1%;
+
+		& a {
+			margin: 0 0;
+			padding-inline-end: 0.5em;
+		}
+	}
+	hr {
+		width: 2em;
+		margin-left: 0;
+	}
+
+	p {
+		color: #FFF;
+		margin: 0;
+	}
+
+	span {
+		color: #ff5650;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,8 +17,8 @@
 
 <script>
 	import "../app.postcss";
-    import Footer from "$lib/Footer.svelte";
+    import Header from "$lib/Header.svelte";
 </script>
 
+<Header />
 <slot />
-<Footer />
