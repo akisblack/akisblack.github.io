ace315f40ce452a6ecf65b876ed3b130d4c49a2b
akisblack <mavrommatisakis@gmail.com>
2021-09-01 20:08:45 +0300

    i am an idiot as well

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,14 @@
-<footer>
+<header>
 	<a href="/"><i class="mdi mdi-home-outline"></i> Home</a>
 	<a href="../bio"><i class="mdi mdi-help-circle-outline"></i> Bio</a>
 	<a href="../projects"><i class="mdi mdi-wrench-outline"></i> Projects</a>
 	<a href="../blog"><i class="mdi mdi-book-outline"></i> Blog</a>
 	<hr>
 	<p>Made with <a href="https://kit.svelte.dev">SvelteKit</a>and <span>&lt;3</span> by Akis.</p>
-</footer>
+</header>
 
 <style lang="postcss">
-	footer {
+	header {
 		width: 99%;
 		background-color: var(--pfp-color);
 		padding: 1em 0 1em 1%;
