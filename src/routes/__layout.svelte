49422c7aad9ba845019c065f6f4b4e905a76f0bd
akisblack <mavrommatisakis@gmail.com>
2021-12-05 19:00:25 +0200

    fix shit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-        <meta charset="utf-8" />
+	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<link rel="shortcut icon" href="/img/logo.png" type="image/x-icon">
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <div class="container text-center">
-	<h1 class="text-accent-secondary text-6xl border-b-4 w-min m-0-auto border-bg-secondary">
+	<h1 class="text-accent-secondary text-6xl w-min m-0-auto">
 		akis<span class="text-accent-primary">black</span>
+		<hr class="border-b-4 border-accent-primary rounded-[1.5px]" />
 	</h1>
 	<h2 class="text-greyish mt-2 text-3xl font-medium">
 		Greek teenager interested in web development
