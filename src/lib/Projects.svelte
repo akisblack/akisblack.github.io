a7866bfbac97baadd211c1e1b414defaa0e285e5
akisblack <mavrommatisakis@gmail.com>
2021-12-31 08:30:20 +0200

    get project data from github api instead of local json

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,5 +1,12 @@
 <script>
-	import projects from "$lib/Projects.json";
+	let repos = [];
+
+	async function fetchRepos() {
+		const url = `https://api.github.com/users/akisblack/repos`;
+		const response = await fetch(url)
+		repos = await response.json();
+	}
+
 	import Icon from "@iconify/svelte";
 	import { Card, InfoButtonsPanel, InfoButton, InfoButtonLink, Description } from "$lib/CardComp";
 </script>
@@ -8,16 +15,31 @@
 	<h1 class="text-accent-secondary max-w-min flex items-center mt-6 self-start">
 		<Icon icon="feather:code" inline={true} class="mr-2" /> Projects
 	</h1>
-	<div>
-		{#each projects as { url, name, description, meta: { lang, color, license } }}
-			<Card {name} {color}>
-				<InfoButtonsPanel slot="btn">
-					<InfoButton tip="Made with {lang}" icon="feather:bookmark" />
-					<InfoButton tip="{license} License" icon="lucide:scale" />
-					<InfoButtonLink tip="GitHub" icon="feather:github" {url} />
-				</InfoButtonsPanel>
-				<Description slot="desc">{description}</Description>
-			</Card>
+	<div on:show={fetchRepos()}>
+		{#each repos as { html_url, name, description, language, license, archived, fork } }
+			{#if (
+					name == "akisblack.github.io" || 
+					name == "browser-copypasta" || 
+					name == "svelte-blog" || 
+					name == "CSS-Snippets" || 
+					name == "svelte-personal-site-template"
+				)
+			}
+				<Card {name} >
+					<InfoButtonsPanel slot="btn">
+						{#if archived}
+							<InfoButton tip="Archived" icon="feather:archive" />
+						{/if}
+						{#if fork}
+							<InfoButton tip="Forked" icon="octicon:repo-forked-16" />
+						{/if}
+						<InfoButton tip="Made with {language}" icon="feather:bookmark" />
+						<InfoButton tip={license?.name} icon="lucide:scale" />
+						<InfoButtonLink tip="GitHub" icon="feather:github" url={html_url} />
+					</InfoButtonsPanel>
+					<Description slot="desc">{description}</Description>
+				</Card>
+			{/if}
 		{/each}
 	</div>
 </div>
