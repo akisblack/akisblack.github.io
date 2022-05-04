9cec733608c0df8f2eb79d2d619fff6c1cff6097
akisblack <mavrommatisakis@gmail.com>
2022-05-04 18:32:29 +0300

    fix card hover

--- src/lib/Card/Card.svelte
+++ src/lib/Card/Card.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class="bg-secondary hover:(-translate-x-1 shadow-xl) my-10 -translate-y-1 rounded-lg border-l-4 p-4 transition-all"
+	class="bg-secondary hover:(-translate-x-1 -translate-y-1 shadow-xl) my-10 rounded-lg border-l-4 p-4 transition-all"
 	style="border-color: {color};"
 >
 	<div class="flex items-center pb-2">
