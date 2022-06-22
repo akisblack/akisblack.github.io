860a1ae7bb957126c829f58c7f5a4b462e471486
Akis <mavrommatisakis@gmail.com>
2022-06-22 13:50:27 +0300

    add public key for email

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -11,7 +11,7 @@
 <div class="contact">
 	<h1 class="mt-6 flex max-w-min items-center self-start">Contact</h1>
 	<div>
-		{#each contact as { url, title, color, username }}
+		{#each contact as { url, title, color, username, publicKey }}
 			<Card name={title} {color}>
 				<Description slot="desc">{username}</Description>
 				<InfoButtonsPanel slot="btn">
@@ -20,6 +20,13 @@
 						icon="i-ic:round-open-in-new"
 						{url}
 					/>
+					{#if publicKey}
+						<InfoButtonLink
+							tip="Public GPG key"
+							icon="i-ic:outline-vpn-key"
+							url={publicKey}
+						/>
+					{/if}
 				</InfoButtonsPanel>
 			</Card>
 		{/each}
