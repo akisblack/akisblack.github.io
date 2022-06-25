f35da67a33bf79811eef6f924bb4b3a0e61b607c
Akis <mavrommatisakis@gmail.com>
2022-06-25 08:17:57 +0300

    use pinned items for projects list

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	let repos = [];
 	async function fetchRepos() {
-		const url = `https://api.github.com/users/akisblack/repos`;
+		const url = `https://gh-pinned-repos.egoist.sh/?username=akisblack`;
 		const response = await fetch(url);
 		repos = await response.json();
 	}
@@ -18,43 +18,23 @@
 <div class="projects">
 	<h1 class="mt-6 flex max-w-min items-center self-start">Projects</h1>
 	<div on:show={fetchRepos()}>
-		{#each repos as { html_url, name, description, language, license, archived, fork }}
-			{#if name === "dark-discord-vscode" || name === "browser-copypasta" || name === "svelte-blog"}
-				<Card {name}>
-					<InfoButtonsPanel slot="btn">
-						{#if archived}
-							<InfoButton
-								tip="Archived"
-								icon="i-octicon:archive-16"
-							/>
-						{/if}
-						{#if fork}
-							<InfoButton
-								tip="Forked"
-								icon="i-octicon:repo-forked-16"
-							/>
-						{/if}
-						{#if language}
-							<InfoButton
-								tip="Made with {language}"
-								icon="i-octicon:bookmark-16"
-							/>
-						{/if}
-						{#if license}
-							<InfoButton
-								tip={license?.name}
-								icon="i-octicon:law-16"
-							/>
-						{/if}
-						<InfoButtonLink
-							tip="GitHub"
-							icon="i-simple-icons:github"
-							url={html_url}
+		{#each repos as { link, owner, repo, description, language, languageColor }}
+			<Card name="{owner}/{repo}" color={languageColor}>
+				<InfoButtonsPanel slot="btn">
+					{#if language}
+						<InfoButton
+							tip="Made with {language}"
+							icon="i-octicon:bookmark-16"
 						/>
-					</InfoButtonsPanel>
-					<Description slot="desc">{description}</Description>
-				</Card>
-			{/if}
+					{/if}
+					<InfoButtonLink
+						tip="GitHub"
+						icon="i-simple-icons:github"
+						url={link}
+					/>
+				</InfoButtonsPanel>
+				<Description slot="desc">{description}</Description>
+			</Card>
 		{/each}
 	</div>
 	<hr class="text-greyish" />
